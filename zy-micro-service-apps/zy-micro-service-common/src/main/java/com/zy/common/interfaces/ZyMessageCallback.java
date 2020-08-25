ccdf7a12f10c7c52f07bb50a2d823b41b3d489df
zhangyu <13100650510@163.com>
2020-08-25 13:58:33 +0800

    feat:common-service utils&kits init

---
+++ zy-micro-service-apps/zy-micro-service-common/src/main/java/com/zy/common/interfaces/ZyMessageCallback.java
@@ -0,0 +1,22 @@
+package com.zy.common.interfaces;
+
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+
+/**
+ * @Author: ZY
+ * @Date: 2019/8/2 15:40
+ * @Version 1.0
+ */
+public interface ZyMessageCallback {
+    /**
+     * Confirmation callback.
+     *
+     * @param correlationData
+     *         correlation data for the callback.
+     * @param ack
+     *         true for ack, false for nack
+     * @param cause
+     *         An optional cause, for nack, when available, otherwise null.
+     */
+    void confirm(CorrelationData correlationData, boolean ack, String cause);
+}
