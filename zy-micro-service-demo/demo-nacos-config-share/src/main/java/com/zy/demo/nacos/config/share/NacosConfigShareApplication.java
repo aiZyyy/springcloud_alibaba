4b1f4ff1365216fab3fd7538a0e7c4e7cac02eac
zhangyu <13100650510@163.com>
2020-09-18 14:32:32 +0800

    feat:Configuration template init

--- zy-micro-service-demo/demo-nacos-config-share/src/main/java/com/zy/demo/nacos/config/share/NacosConfigShareApplication.java
+++ zy-micro-service-demo/demo-nacos-config-share/src/main/java/com/zy/demo/nacos/config/share/NacosConfigShareApplication.java
@@ -21,12 +21,8 @@ public class NacosConfigShareApplication {
     @Value("${nacos.share}")
     private String share;
 
-
-    @Value("${share.config1}")
+    @Value("${nacos}")
     private String shareConfig1;
-
-    @Value("${share.config2}")
-    private String shareConfig2;
 
     @RequestMapping("/getValue")
     public String getValue() {
@@ -36,29 +32,5 @@ public class NacosConfigShareApplication {
     @RequestMapping("/getShare1")
     public String getShare1() {
         return shareConfig1;
-    }
-
-    @RequestMapping("/getShare2")
-    public String getShare2() {
-        return shareConfig2;
-    }
-
-
-
-    @Value("${share.config3}")
-    private String shareConfig3;
-
-    @Value("${share.config4}")
-    private String shareConfig4;
-
-
-    @RequestMapping("/getShare3")
-    public String getShare3() {
-        return shareConfig3;
-    }
-
-    @RequestMapping("/getShare4")
-    public String getShare4() {
-        return shareConfig4;
     }
 }
