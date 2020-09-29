a99ee89647c3c6feb672814c4d78c0b9f38b16de
zhangyu <13100650510@163.com>
2020-09-29 18:04:40 +0800

    feat:base-oauth-service init

---
+++ zy-micro-services-base/base-oauth-service/src/main/java/com/zy/base/oauthservice/BaseOauthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zy.base.oauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseOauthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseOauthServiceApplication.class, args);
+    }
+
+}
---
+++ zy-micro-services-base/base-oauth-service/src/main/java/com/zy/base/oauthservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.zy.base.oauthservice.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import com.zy.base.oauthservice.constant.MessageConstant;
+import com.zy.base.oauthservice.entity.domain.SecurityUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.zy.apps.common.domain.entity.UserDto;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author ZY
+ * @date 2020/9/29 17:11
+ * @Description:
+ * @Version 1.0
+ */
+@Service
+public class UserServiceImpl implements UserDetailsService {
+    private List<UserDto> userList;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initData() {
+        String password = passwordEncoder.encode("123456");
+        userList = new ArrayList<>();
+        userList.add(new UserDto(1L, "zy", password, 1, "1", CollUtil.toList("ADMIN")));
+        userList.add(new UserDto(2L, "zyy", password, 1, "1", CollUtil.toList("TEST")));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDto> findUserList = userList.stream().filter(item -> item.getUsername().equals(username)).collect(Collectors.toList());
+        if (CollUtil.isEmpty(findUserList)) {
+            throw new UsernameNotFoundException(MessageConstant.USERNAME_PASSWORD_ERROR);
+        }
+        SecurityUser securityUser = new SecurityUser(findUserList.get(0));
+        if (!securityUser.isEnabled()) {
+            throw new DisabledException(MessageConstant.ACCOUNT_DISABLED);
+        } else if (!securityUser.isAccountNonLocked()) {
+            throw new LockedException(MessageConstant.ACCOUNT_LOCKED);
+        } else if (!securityUser.isAccountNonExpired()) {
+            throw new AccountExpiredException(MessageConstant.ACCOUNT_EXPIRED);
+        } else if (!securityUser.isCredentialsNonExpired()) {
+            throw new CredentialsExpiredException(MessageConstant.CREDENTIALS_EXPIRED);
+        }
+        return securityUser;
+    }
+}
